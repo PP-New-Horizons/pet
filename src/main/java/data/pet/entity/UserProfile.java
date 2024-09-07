3fd705679a668fc20ae5717196a6db08d358dd40
TokarevMik <stiks84@gmail.com>
2024-09-08 02:25:16 +0500

    rename user class#8695p0q4d

--- src/main/java/data/pet/entity/UserProfile.java
+++ src/main/java/data/pet/entity/UserProfile.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class User {
+public class UserProfile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
