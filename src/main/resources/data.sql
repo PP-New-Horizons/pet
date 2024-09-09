b37c567c08137a3cf13f8085795c7869b3f2a826
Nataliya <78302372+nataliya-nataliya@users.noreply.github.com>
2024-09-09 19:00:08 +0100

    #8695p848n change and add data of tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,10 +13,13 @@ INSERT INTO health_type (name) VALUES ('С ограниченными возмо
 INSERT INTO health_type (name) VALUES ('Требуется лечение') ON CONFLICT (name) DO NOTHING;
 INSERT INTO health_type (name) VALUES ('Хорошее') ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO health_type (name) VALUES ('Гладкошерстные') ON CONFLICT (name) DO NOTHING;
-INSERT INTO health_type (name) VALUES ('Длинношерстные') ON CONFLICT (name) DO NOTHING;
-INSERT INTO health_type (name) VALUES ('Лысые') ON CONFLICT (name) DO NOTHING;
+INSERT INTO hair (name) VALUES ('Гладкошерстный') ON CONFLICT (name) DO NOTHING;
+INSERT INTO hair (name) VALUES ('Длинношерстный') ON CONFLICT (name) DO NOTHING;
+INSERT INTO hair (name) VALUES ('Лысый') ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO size (name) VALUES ('Большие') ON CONFLICT (name) DO NOTHING;
-INSERT INTO size (name) VALUES ('Средние') ON CONFLICT (name) DO NOTHING;
-INSERT INTO size (name) VALUES ('Маленькие') ON CONFLICT (name) DO NOTHING;
+INSERT INTO size (name) VALUES ('Большой') ON CONFLICT (name) DO NOTHING;
+INSERT INTO size (name) VALUES ('Средний') ON CONFLICT (name) DO NOTHING;
+INSERT INTO size (name) VALUES ('Маленький') ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO pet_type (name) VALUES ('Кошка') ON CONFLICT (name) DO NOTHING;
+INSERT INTO pet_type (name) VALUES ('Собака') ON CONFLICT (name) DO NOTHING;
